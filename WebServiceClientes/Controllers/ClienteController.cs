800b5cf507c8fb495f008619fe68267886641ee7
BryanFlechas25 <68081485+BryanFlechas25@users.noreply.github.com>
2021-03-28 00:46:19 -0500

    Creacion Repositorio

---
+++ WebServiceClientes/Controllers/ClienteController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebServiceClientes.Models;
+
+namespace WebServiceClientes.Controllers
+{
+    public class ClienteController : ApiController
+    {
+        CAR_CENTEREntitie Bd = new CAR_CENTEREntitie();
+
+        // GET api/Cliente
+        public IHttpActionResult GetClientes()
+        {
+            try
+            {
+                Bd.Configuration.ProxyCreationEnabled = false;
+                var clientes = Bd.Cliente.ToList();
+                return Ok(clientes);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        //GET api/Cliente/{id}
+        public IHttpActionResult GetCliente(int id)
+        {
+            try
+            {
+                Bd.Configuration.ProxyCreationEnabled = false;
+                Cliente cliente = Bd.Cliente.Find(id);
+                if (cliente == null)
+                {
+                    return BadRequest("Record not found");
+                }
+                return Ok(cliente);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        // POST api/Cliente
+        public IHttpActionResult Addcliente(Cliente model)
+        {
+            //ModelState ayuda a comprobar el estado del modelo si coincide con los datanotation de nuestra partialclass
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Bd.Cliente.Add(model);
+                Bd.SaveChanges();
+                return Ok("Success");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+
+        }
+
+        // PUT api/Client/{id}
+        public IHttpActionResult Put(int id, Cliente model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                Cliente cliente = Bd.Cliente.Find(id);
+                if (cliente == null)
+                {
+                    return BadRequest("Record not found");
+                }
+                Bd.Entry(cliente).State = EntityState.Detached;
+                Bd.Entry(model).State = EntityState.Modified;
+                Bd.SaveChanges();
+                return Ok("Success");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        // DELETE api/Cliente/{id}
+        public IHttpActionResult Deletecliente(int id)
+        {
+            try
+            {
+                Cliente cliente = Bd.Cliente.Find(id);
+                if (cliente == null)
+                {
+                    return BadRequest("Record not found");
+                }
+                Bd.Cliente.Remove(cliente);
+                Bd.SaveChanges();
+                return Ok("Success");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ WebServiceClientes/Models/ClienteModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceClientes.Models
+{
+    public class ClienteModel
+    {
+        public int Id_cliente { get; set; }
+        public string Tipo_de_documento { get; set; }
+        public string Documento { get; set; }
+        public string Primer_nombre { get; set; }
+        public string Segundo_nombre { get; set; }
+        public Nullable<int> Celular { get; set; }
+        public string Dirección { get; set; }
+        public string Correo_electronico { get; set; }
+    }
+}
